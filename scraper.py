266d2dd56fd55366d2c35df573a31a2194f64505
Igor Czerwinski <czerwony@gmail.com>
2011-11-20 23:39:37 +0100

    swedish scraper

---
+++ scraper.py
@@ -0,0 +1,58 @@
+#!/bin/python2.7
+
+#//span[@id='Swedish']/../following-sibling::ol/li[1]/child::node()
+#//span[@id='Swedish']/../following-sibling::div[@class='NavFrame']/div[@class='NavContent']/table/tbody/tr[3]/td
+#//span[@id='Swedish']/../following-sibling::p/span/i/child::text()
+
+#http://en.wiktionary.org/wiki/
+
+import urllib2
+import codecs
+from lxml import etree
+
+ao = u"\u00e5".encode("utf-8")
+ae = u"\u00e4".encode("utf-8")
+oe = u"\u00f6".encode("utf-8")
+
+class Word():
+	gender = ""
+	word = ""
+	definition = ""
+	declension = ()
+	def __str__(self):
+		ret = "%(gender)s %(word)s -- %(definition)s\n" % {"gender":self.gender, "word": self.word, "definition": self.defintion}
+		ret += " ".join(self.declension)
+		return ret	
+
+def getDefinition(word):
+	url = "".join(["http://en.wiktionary.org/wiki/",word])
+	user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+	headers = { 'User-Agent' : user_agent, 'Content-Type' : "charset=UTF-8" }
+	req = urllib2.Request(url=url, headers=headers)
+	html = codecs.EncodedFile(urllib2.urlopen(req), "utf-8")
+	tree = etree.HTML(html.read())
+
+	w = Word()
+
+	gender = tree.xpath("//span[@id='Swedish']/../following-sibling::p/span/i/child::text()")
+	if gender[0] == "c":
+		w.gender = "en"
+	else:
+		w.gender = "ett"
+	
+	w.word = word
+	
+	definition = tree.xpath("//span[@id='Swedish']/../following-sibling::ol/li[1]")
+	w.defintion = definition[0].xpath("string()")
+
+	declension = tree.xpath("//span[@id='Swedish']/../following-sibling::div[@class='NavFrame']/div[@class='NavContent']/table/tr[3]/td")
+	w.declension = tuple([d.xpath("string()").encode("utf-8") for d in declension])
+
+	return w
+
+words = [ao+"r", "kapitel", "pojke"]
+
+for word in words:
+	print getDefinition(word)
+	print
+
